7bba5cd39e4b4b15774cf55852099e5895948a8e
osidekyle <hawkinskyle67@gmail.com>
2020-06-25 21:13:42 -0700

    learning basic jquery animations, events, selecting elements w/ ids, classes, attributes

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+
 $(document).ready(function(){
-    $('#panel').hide(300).show(300); 
+
+    /*Basic Showing and Hiding
+    $('#panel').css({
+        color:"red",
+        fontWeight:"bold",
+
+        
+    }).fadeOut(1000)
+    $("#panel2").css({
+        opacity:".5"
+    })
+    */
+
+    /*Changing HTML
+    $('#panel').html('<h2>dang</h2>');
+    */
+
+    //DRY
+    $(".panel-main").hide();
+    $(".panel-button").on('click',function(){
+        $('#'+$(this).attr('data-panelid')).fadeToggle(200)
+    })
+
+
+
+
 });
 
 
